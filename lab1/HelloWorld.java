88c12a027153e91420893a2cb54646af73fbc952
Gilmore <lihaoxue232@outlook.com>
2019-04-11 10:48:58 +0800

    completed first part of lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(10+5);
+    }
+}
+
+/*
+1. All code in Java must be part of a class;
+2. We delimit the beginning and edning of segments of code using
+{ and }
+3.All statements in Java must end in a semi-colon
+4.for code to run we need public static void main(String[] args)
+*/
